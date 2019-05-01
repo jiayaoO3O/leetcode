44c3b08930b188da748b07a812e4e68dd1811fbe
jiayao <jiayao3.14@outlook.com>
2019-05-01 23:19:22 +0800

    13.罗马数字转整数

--- Solution.cs
+++ Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace leetcode
 {
@@ -69,6 +70,30 @@ namespace leetcode
                 }
                 return result == x ? true : false;
             }
+        }
+
+        //13.罗马数字转整数
+        //https://leetcode-cn.com/problems/roman-to-integer/comments/
+        public int RomanToInt(string s)
+        {
+
+            Dictionary<char, int> romanNumber = new Dictionary<char, int> {
+                { 'I', 1 },{ 'V', 5 },{ 'X', 10 },{ 'L', 50 },
+                { 'C', 100 },{ 'D', 500 },{ 'M', 1000 }
+            };
+            int result = romanNumber[s[s.Length - 1]];
+            for (int i = s.Length - 2; i >= 0; i--)
+            {
+                if (romanNumber[s[i]] < romanNumber[s[i + 1]])
+                {
+                    result -= romanNumber[s[i]];
+                }
+                else
+                {
+                    result += romanNumber[s[i]];
+                }
+            }
+            return result;
         }
     }
 }
