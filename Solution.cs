9258fefca05fde3d2b5c1ae3e3e8738ea830c8db
jiayao <jiayao3.14@outlook.com>
2019-05-01 19:40:13 +0800

    9.回文数

--- Solution.cs
+++ Solution.cs
@@ -39,5 +39,36 @@ namespace leetcode
             }
             return result;
         }
+
+        //9.回文数
+        //https://leetcode-cn.com/problems/palindrome-number/
+        public bool IsPalindrome(int x)
+        {
+            //直接转换字符串.
+            // string str = x.ToString();
+            // for (int i = 0; i < str.Length / 2; i++)
+            // {
+            //     if (str[i] != str[str.Length - i - 1])
+            //     {
+            //         return false;
+            //     }
+            // }
+            // return true;
+            if (x < 0)
+            {
+                return false;
+            }
+            else
+            {
+                int num = x;
+                int result = 0;
+                while (num != 0)
+                {
+                    result = result * 10 + num % 10;
+                    num /= 10;
+                }
+                return result == x ? true : false;
+            }
+        }
     }
 }
