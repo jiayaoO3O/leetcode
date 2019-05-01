a0641e0779453aa2bc873420000bc66ab07f9dd1
jiayao <jiayao3.14@outlook.com>
2019-05-01 17:24:36 +0800

    7.整数反转

--- Solution.cs
+++ Solution.cs
@@ -20,5 +20,24 @@ namespace leetcode
             }
             return new int[] { 0, 0 };
         }
+
+        //7.整数反转
+        //https://leetcode-cn.com/problems/reverse-integer/
+        public int Reverse(int x)
+        {
+            int n = 0;
+            int result = 0;
+            while (x != 0)
+            {
+                result = n * 10 + x % 10;
+                if (result / 10 != n)//不等于n说明溢出了.
+                {
+                    return 0;
+                }
+                x /= 10;
+                n = result;
+            }
+            return result;
+        }
     }
 }
