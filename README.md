e335c6e0030b896f565cb30694a04f8838bfcb0f
jiayao <jiayao3.14@outlook.com>
2019-05-03 16:03:12 +0800

    20.有效的括号

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 |  9   | 简单 |  [回文数](https://jiayaoo3o.github.io/2019/05/01/9-回文数/)  |
 |  13  | 简单 | [罗马数字转整数](https://jiayaoo3o.github.io/2019/05/01/13-罗马数字转整数/) |
 |  14  | 简单 | [最长公共前缀](https://jiayaoo3o.github.io/2019/05/02/14-最长公共前缀/) |
+|  20  | 简单 | [有效的括号](https://jiayaoo3o.github.io/2019/05/03/20-有效的括号/) |
 
--- Solution.cs
+++ Solution.cs
@@ -120,5 +120,36 @@ namespace leetcode
             }
             return strs[0];
         }
+
+        //20.有效的括号
+        //https://leetcode-cn.com/problems/valid-parentheses/
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack.Push(s[i]);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        char symbol = stack.Pop();
+                        if (s[i] == ')' && symbol != '(' || s[i] == ']' && symbol != '[' || s[i] == '}' && symbol != '{')
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+            }
+            return stack.Count == 0;
+        }
     }
 }
