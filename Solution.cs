7c63cddbac7d0ad38ba91d4e192a4a2d0d6b9dcc
jiayao <jiayao3.14@outlook.com>
2019-05-04 23:38:35 +0800

    27.移除元素

--- Solution.cs
+++ Solution.cs
@@ -201,5 +201,25 @@ namespace leetcode
             }
             return i + 1;
         }
+
+        //27.移除元素
+        //https://leetcode-cn.com/problems/remove-element/
+        public int RemoveElement(int[] nums, int val)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n;)
+            {
+                if (nums[i] == val)
+                {
+                    n--;
+                    nums[i] = nums[n];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return n;
+        }
     }
 }
