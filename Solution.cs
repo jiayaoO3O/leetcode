9e3b6b6a4d35d29bff108935514ec35533e11298
jiayao <jiayao3.14@outlook.com>
2019-05-04 17:19:11 +0800

    26.删除排序数组中的重复项

--- Solution.cs
+++ Solution.cs
@@ -181,5 +181,25 @@ namespace leetcode
             node.next = l1 == null ? l1 : l2;
             return head.next;
         }
+
+        //26.删除排序数组中的重复项
+        //https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/submissions/
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])//用快慢指针解决,后面的数覆盖前面的重复值.
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
     }
 }
