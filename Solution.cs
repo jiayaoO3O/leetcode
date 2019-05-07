03b573b6f0b4c5c93e84d1839a74e9d9038dc076
jiayao <jiayao3.14@outlook.com>
2019-05-07 08:52:40 +0800

    66.加一

--- Solution.cs
+++ Solution.cs
@@ -269,5 +269,30 @@ namespace leetcode
         {
             return s.Trim().Split()[s.Trim().Split().Length - 1].Length;
         }
+
+        //66.加一
+        //https://leetcode-cn.com/problems/plus-one/
+        public int[] PlusOne(int[] digits)
+        {
+            int flag = 1;
+            int digitSum = 0;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                digitSum = digits[i] + flag;
+                digits[i] = digitSum % 10;
+                flag = (digitSum + 1 + flag) / 10;
+            }
+            if (flag == 0)
+            {
+                return digits;
+            }
+            else
+            {
+                int[] result = new int[digits.Length + 1];
+                result[0] = 1;
+                digits.CopyTo(result, 1);
+                return result;
+            }
+        }
     }
 }
