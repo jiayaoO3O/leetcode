d229e99697c9914891069d7bbda6822bea2aa282
jiayao <jiayao3.14@outlook.com>
2019-05-08 12:50:57 +0800

    70.爬楼梯

--- README.md
+++ README.md
@@ -18,4 +18,5 @@
 |  66  | 简单 |   [加一](https://jiayaoo3o.github.io/2019/05/07/66-加一/)    |
 |  67  | 简单 | [二进制求和](https://jiayaoo3o.github.io/2019/05/07/67-二进制求和/) |
 |  69  | 简单 | [x的平方分根](https://jiayaoo3o.github.io/2019/05/07/69-x的平方分根/) |
+|  70  | 简单 | [爬楼梯](https://jiayaoo3o.github.io/2019/05/08/70-爬楼梯/)  |
 
--- Solution.cs
+++ Solution.cs
@@ -334,5 +334,33 @@ namespace leetcode
             }
             return (int)r;
         }
+
+        //70.爬楼梯
+        //https://leetcode-cn.com/problems/climbing-stairs/
+        //这一题是很典型的动态规划问题,可以去看看这个推文:
+        //https://mp.weixin.qq.com/s/3h9iqU4rdH3EIy5m6AzXsg
+        public int ClimbStairs(int n)
+        {
+            //if (n==1||n==0)//递归操作;
+            //{
+            //    return 1;
+            //}
+            //else
+            //{
+            //    return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+            //}
+            if (n == 1 || n == 0 || n == 2)
+            {
+                return n;
+            }
+            int[] r = new int[n + 1];//定义一个数组,接收每一层楼梯的走法
+            r[1] = 1;
+            r[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                r[i] = r[i - 1] + r[i - 2];
+            }
+            return r[n];
+        }
     }
 }
