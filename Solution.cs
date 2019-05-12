abacfdafae9f6497c04cf32e99b9912b37e4c339
jiayao <jiayao3.14@outlook.com>
2019-05-12 11:49:38 +0800

    121.买卖股票的最佳时机

--- Solution.cs
+++ Solution.cs
@@ -446,5 +446,22 @@ namespace leetcode
                 return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
             }
         }
+
+        //121.买卖股票的最佳时机
+        //https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length <= 0)
+            {
+                return 0;
+            }
+            int minPrice = prices[0], maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+                minPrice = Math.Min(minPrice, prices[i]);
+            }
+            return maxProfit;
+        }
     }
 }
