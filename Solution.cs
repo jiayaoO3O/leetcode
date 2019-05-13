97d1f59bd4a0d2402c1739b60dc01bb4e8688550
jiayao <jiayao3.14@outlook.com>
2019-05-13 10:28:42 +0800

    136.只出现一次的数字

--- Solution.cs
+++ Solution.cs
@@ -486,5 +486,17 @@ namespace leetcode
             }
             return true;
         }
+
+        //136.只出现一次的数字
+        //https://leetcode-cn.com/problems/single-number/
+        public int SingleNumber(int[] nums)
+        {
+            int n = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                n = n ^ nums[i];
+            }
+            return n;
+        }
     }
 }
