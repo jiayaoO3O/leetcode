d848f72baf393916e910400d6de1851cdeb6c1bb
jiayao <jiayao3.14@outlook.com>
2019-05-13 10:45:38 +0800

    141.环形链表

--- Solution.cs
+++ Solution.cs
@@ -498,5 +498,23 @@ namespace leetcode
             }
             return n;
         }
+
+        //141.环形链表
+        //https://leetcode-cn.com/problems/linked-list-cycle/
+        public bool HasCycle(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
