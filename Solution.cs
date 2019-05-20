6247ebad063e8442b028715ccc3e5b2e61c7512d
jiayao <jiayao3.14@outlook.com>
2019-05-20 11:18:00 +0800

    202.快乐数

--- Solution.cs
+++ Solution.cs
@@ -724,5 +724,29 @@ namespace leetcode
             }
             return sum[sum.Length - 1];
         }
+
+        //202.快乐数
+        //https://leetcode-cn.com/problems/happy-number/
+        public bool IsHappy(int n)
+        {
+            int result = 0;
+            HashSet<int> used = new HashSet<int>();
+            while (!used.Contains(result))
+            {
+                used.Add(result);
+                result = 0;
+                while (n != 0)
+                {
+                    result = result + (n % 10) * (n % 10);
+                    n = n / 10;
+                }
+                if (result == 1)
+                {
+                    return true;
+                }
+                n = result;
+            }
+            return false;
+        }
     }
 }
