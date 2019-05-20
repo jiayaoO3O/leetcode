77197ec1d63faad9599e3a0dc001fe47c0ff2cb9
jiayao <jiayao3.14@outlook.com>
2019-05-20 10:44:33 +0800

    198.打家劫舍

--- Solution.cs
+++ Solution.cs
@@ -702,5 +702,27 @@ namespace leetcode
             }
             return result;
         }
+
+        //198.打家劫舍
+        //https://leetcode-cn.com/problems/house-robber/
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+            int[] sum = new int[nums.Length];
+            sum[0] = nums[0];
+            sum[1] = Math.Max(nums[0], nums[1]);
+            for (int i = 2; i < sum.Length; i++)
+            {
+                sum[i] = Math.Max(sum[i - 1], sum[i - 2] + nums[i]);
+            }
+            return sum[sum.Length - 1];
+        }
     }
 }
