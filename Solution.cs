937295ae40646baf42067fa64c8a70f1114540ed
jiayao <jiayao3.14@outlook.com>
2019-05-22 22:11:35 +0800

    203.删除链表中的节点

--- Solution.cs
+++ Solution.cs
@@ -748,5 +748,39 @@ namespace leetcode
             }
             return false;
         }
+
+        //203.删除链表中的节点
+        //https://leetcode-cn.com/problems/remove-linked-list-elements/
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            // ListNode p = new ListNode(-1)
+            // {
+            //     next = head
+            // };
+            // ListNode h = p;
+            // while (p.next != null)
+            // {
+            //     if (p.next.val == val)
+            //     {
+            //         p.next = p.next.next;
+            //         continue;
+            //     }
+            //     p = p.next;
+            // }
+            // return h.next;
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.val == val)
+            {
+                return RemoveElements(head.next, val);
+            }
+            else
+            {
+                head.next = RemoveElements(head.next, val);
+            }
+            return head;
+        }
     }
 }
