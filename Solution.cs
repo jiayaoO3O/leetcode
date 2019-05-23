369c5205deebcb72cc012bab60f34c600872c061
jiayao <jiayao3.14@outlook.com>
2019-05-23 21:40:17 +0800

    205.同构字符串

--- Solution.cs
+++ Solution.cs
@@ -782,5 +782,31 @@ namespace leetcode
             }
             return head;
         }
+
+        //205.同构字符串
+        //https://leetcode-cn.com/problems/isomorphic-strings/
+        public bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> strInfo = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!strInfo.ContainsKey(s[i]))
+                {
+                    if (strInfo.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+                    strInfo.Add(s[i], t[i]);
+                }
+                else
+                {
+                    if (strInfo[s[i]] != t[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
