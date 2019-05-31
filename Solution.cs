ae423cdd57066e75c420f48106f69ba39a74034c
jiayao <jiayao3.14@outlook.com>
2019-05-31 16:36:46 +0800

    237.删除链表中的节点

--- Solution.cs
+++ Solution.cs
@@ -968,5 +968,14 @@ namespace leetcode
             }
             return true;
         }
+
+        //237.删除链表中的节点
+        //https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+        //神题.
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;//用下一个节点的数据覆盖当前节点.
+            node.next = node.next.next;//删除下一个节点.
+        }
     }
 }
