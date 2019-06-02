b0ffa25f77ed0d2e194d977cf7fa08c952674d73
jiayao <jiayao3.14@outlook.com>
2019-06-02 22:27:42 +0800

    258.各位相加

--- Solution.cs
+++ Solution.cs
@@ -1026,5 +1026,36 @@ namespace leetcode
             }
             return charDict.Count == 0;
         }
+
+        //258.各位相加
+        //https://leetcode-cn.com/problems/add-digits/
+        public int AddDigits(int num)
+        {
+            if (num > 9)
+            {
+                num = num % 9;
+                if (num == 0)
+                {
+                    return 9;
+                }
+            }
+            return num;
+            // if (num < 10)
+            // {
+            //     return num;
+            // }
+            // int result = 0;
+            // while (num >= 10)
+            // {
+            //     result = 0;
+            //     while (num != 0)
+            //     {
+            //         result += num % 10;
+            //         num /= 10;
+            //     }
+            //     num = result;
+            // }
+            // return num;
+        }
     }
 }
