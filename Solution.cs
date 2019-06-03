255ba7c712a4c53738478aa149c87ef00827441e
jiayao <jiayao3.14@outlook.com>
2019-06-03 17:28:58 +0800

    263.丑数

--- Solution.cs
+++ Solution.cs
@@ -1057,5 +1057,28 @@ namespace leetcode
             // }
             // return num;
         }
+
+        //263.丑数
+        //https://leetcode-cn.com/problems/ugly-number/
+        public bool IsUgly(int num)
+        {
+            if (num <= 1)
+            {
+                return (num == 1) ? true : false;
+            }
+            while (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            while (num % 3 == 0)
+            {
+                num /= 3;
+            }
+            while (num % 5 == 0)
+            {
+                num /= 5;
+            }
+            return num == 1;
+        }
     }
 }
