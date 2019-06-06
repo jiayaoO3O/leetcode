cdfc6841bd1c132a6bdb453f03d6c45b234501fc
jiayao <jiayao3.14@outlook.com>
2019-06-06 12:59:22 +0800

    268.缺失数字

--- Solution.cs
+++ Solution.cs
@@ -1080,5 +1080,26 @@ namespace leetcode
             }
             return num == 1;
         }
+
+        //268.缺失数字
+        //https://leetcode-cn.com/problems/missing-number/
+        public int MissingNumber(int[] nums)
+        {
+            // Array.Sort(nums);
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     if (i != nums[i])
+            //     {
+            //         return i;
+            //     }
+            // }
+            // return nums[nums.Length - 1] + 1;
+            int sum = (nums.Length * (nums.Length + 1)) / 2;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum -= nums[i];
+            }
+            return sum;
+        }
     }
 }
