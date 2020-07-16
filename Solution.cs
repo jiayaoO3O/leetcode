85d9cc135af0b992393b51be23be7cd06500fb02
jiayao <jiayao3.14@outlook.com>
2020-07-16 09:34:17 +0800

    add Solution0001

--- Solution.cs
+++ Solution.cs
@@ -18,23 +18,6 @@ namespace leetcode
     }
     class Solution
     {
-        //1.两数之和
-        //https://leetcode-cn.com/problems/two-sum/
-        public int[] TwoSum(int[] nums, int target)
-        {
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
-                {
-                    if (nums[i] + nums[j] == target)
-                    {
-                        return new int[] { i, j };
-                    }
-                }
-            }
-            return new int[] { 0, 0 };
-        }
-
         //7.整数反转
         //https://leetcode-cn.com/problems/reverse-integer/
         public int Reverse(int x)
---
+++ TwoSum.cs
@@ -0,0 +1,22 @@
+namespace leetcode
+{
+    class Solution0001
+    {
+        //1.两数之和
+        //https://leetcode-cn.com/problems/two-sum/
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] { 0, 0 };
+        }
+    }
+}
