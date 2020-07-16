216f53fd0a1ed8a33bd4672c74c5b532be6d3755
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:03:00 +0800

    验证回文串

--- Solution.cs
+++ Solution.cs
@@ -20,28 +20,7 @@ namespace leetcode
     {
         
 
-        //125.验证回文串
-        //https://leetcode-cn.com/problems/valid-palindrome/
-        public bool IsPalindrome(string s)
-        {
-            s = s.ToLower();
-            System.Text.StringBuilder result = new System.Text.StringBuilder();
-            foreach (var c in s)
-            {
-                if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9')
-                {
-                    result.Append(c);
-                }
-            }
-            for (int i = 0; i < result.Length / 2; i++)
-            {
-                if (result[i] != result[result.Length - 1 - i])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        
 
         //136.只出现一次的数字
         //https://leetcode-cn.com/problems/single-number/
