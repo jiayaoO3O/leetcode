38a15ec8d6c42465e6d9621ec1e544066b6ce84f
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:39:02 +0800

    第一个错误的版本

--- Solution.cs
+++ Solution.cs
@@ -30,29 +30,8 @@ namespace leetcode
 
         
 
-        //278.第一个错误的版本
-        //https://leetcode-cn.com/problems/first-bad-version/
-        public int FirstBadVersion(int n)
-        {
-            int left = 1, right = n;
-            while (left < right)
-            {
-                int mid = left + (right - left) / 2;
-                if (IsBadVersion(mid))
-                {
-                    right = mid;
-                }
-                else
-                {
-                    left = mid + 1;
-                }
-            }
-            return left;
-        }
-        private bool IsBadVersion(int mid) => throw new NotImplementedException();
-        //这个函数放在这个位置是没有意义的.
-        //唯一的目的是为了让代码不报错.
-        //提交时不需要写这个函数.
+        
+        
 
         //283.移动零
         //https://leetcode-cn.com/problems/move-zeroes/
