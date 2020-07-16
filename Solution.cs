187b69f9895fc5b2cfea39c1cc5f1840593c9710
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:24:04 +0800

    用队列实现栈

--- Solution.cs
+++ Solution.cs
@@ -23,33 +23,7 @@ namespace leetcode
 
         
 
-        //225.用队列实现栈
-        //https://leetcode-cn.com/problems/implement-stack-using-queues/
-        public class MyStack
-        {
-            Queue<int> queue = new Queue<int>();
-            public MyStack() { }
-            public void Push(int x)
-            {
-                queue.Enqueue(x);
-                for (int i = 0; i < queue.Count - 1; i++)
-                {
-                    queue.Enqueue(queue.Dequeue());
-                }
-            }
-            public int Pop()
-            {
-                return queue.Dequeue();
-            }
-            public int Top()
-            {
-                return queue.Peek();
-            }
-            public bool Empty()
-            {
-                return queue.Count > 0 ? false : true;
-            }
-        }
+        
 
         //231.2的幂
         //https://leetcode-cn.com/problems/power-of-two/
