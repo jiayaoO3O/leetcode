c30faffb839a517c947b1174afaeb8634f59bb53
jiayao <jiayao3.14@outlook.com>
2020-07-16 09:54:56 +0800

    add Solution0020

--- Solution.cs
+++ Solution.cs
@@ -25,36 +25,7 @@ namespace leetcode
 
         
 
-        //20.有效的括号
-        //https://leetcode-cn.com/problems/valid-parentheses/
-        public bool IsValid(string s)
-        {
-            Stack<char> stack = new Stack<char>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-                {
-                    stack.Push(s[i]);
-                }
-                else
-                {
-                    if (stack.Count == 0)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        char symbol = stack.Pop();
-                        if (s[i] == ')' && symbol != '(' || s[i] == ']' && symbol != '[' || s[i] == '}' && symbol != '{')
-                        {
-                            return false;
-                        }
-                    }
-                }
-
-            }
-            return stack.Count == 0;
-        }
+        
 
         //21.合并两个有序链表
         //https://leetcode-cn.com/problems/merge-two-sorted-lists/comments/
