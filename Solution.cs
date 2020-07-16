39e28329e5ba564fe3a4a0f2c329f7c0b443bacb
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:12:00 +0800

    求众数

--- Solution.cs
+++ Solution.cs
@@ -19,30 +19,7 @@ namespace leetcode
     class Solution
     {
 
-        //169.求众数
-        //https://leetcode-cn.com/problems/majority-element/
-        public int MajorityElement(int[] nums)
-        {
-            // Array.Sort(nums);
-            // return nums[nums.Length / 2];
-            Dictionary<int, int> numsInfo = new Dictionary<int, int>();
-            foreach (var num in nums)
-            {
-                if (numsInfo.ContainsKey(num))
-                {
-                    numsInfo[num]++;
-                }
-                else
-                {
-                    numsInfo.Add(num, 1);
-                }
-                if (numsInfo[num] > nums.Length / 2)
-                {
-                    return num;
-                }
-            }
-            return 0;
-        }
+        
 
         //172.阶乘后的零
         //https://leetcode-cn.com/problems/factorial-trailing-zeroes/
