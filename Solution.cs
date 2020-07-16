208d4450cbe9dd515f6a439f083c33d27c553906
jiayao <jiayao3.14@outlook.com>
2020-07-16 09:53:47 +0800

    add Solution0014

--- Solution.cs
+++ Solution.cs
@@ -23,30 +23,7 @@ namespace leetcode
 
         
 
-        //14.最长公共前缀
-        //https://leetcode-cn.com/problems/longest-common-prefix/comments/
-        public string LongestCommonPrefix(string[] strs)
-        {
-            if (strs == null || strs.Length == 0)
-            {
-                return "";
-            }
-            if (strs.Length == 1)
-            {
-                return strs[0];
-            }
-            for (int i = 0; i < strs[0].Length; ++i)
-            {
-                for (int j = 1; j < strs.Length; ++j)
-                {
-                    if (strs[j].Length <= i || strs[0][i] != strs[j][i])
-                    {
-                        return strs[0].Substring(0, i);
-                    }
-                }
-            }
-            return strs[0];
-        }
+        
 
         //20.有效的括号
         //https://leetcode-cn.com/problems/valid-parentheses/
