72f8af5a3e33b41ea9cb07ec9000ba6bc7fe76ca
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:32:26 +0800

    删除链表中的节点

--- Solution.cs
+++ Solution.cs
@@ -21,15 +21,7 @@ namespace leetcode
 
         
 
-        //237.删除链表中的节点
-        //https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
-        //神题.
-        public void DeleteNode(ListNode node)
-        {
-            node.val = node.next.val;//用下一个节点的数据覆盖当前节点.
-            node.next = node.next.next;//删除下一个节点.
-        }
-
+        
         //242.有效的字母异位词
         //https://leetcode-cn.com/problems/valid-anagram/
         public bool IsAnagram(string s, string t)
---
+++ Solution0237.cs
@@ -0,0 +1,14 @@
+namespace leetcode
+{
+    class Solution0237
+    {
+        //237.删除链表中的节点
+        //https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;//用下一个节点的数据覆盖当前节点.
+            node.next = node.next.next;//删除下一个节点.
+        }
+
+    }
+}
