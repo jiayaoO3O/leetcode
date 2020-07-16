60f0feb49bc783ebcf7d1e92b6dda18777bc21b3
jiayao <jiayao3.14@outlook.com>
2020-07-16 16:19:36 +0800

    删除链表中的节点

--- Solution.cs
+++ Solution.cs
@@ -28,39 +28,7 @@ namespace leetcode
 
         
 
-        //203.删除链表中的节点
-        //https://leetcode-cn.com/problems/remove-linked-list-elements/
-        public ListNode RemoveElements(ListNode head, int val)
-        {
-            // ListNode p = new ListNode(-1)
-            // {
-            //     next = head
-            // };
-            // ListNode h = p;
-            // while (p.next != null)
-            // {
-            //     if (p.next.val == val)
-            //     {
-            //         p.next = p.next.next;
-            //         continue;
-            //     }
-            //     p = p.next;
-            // }
-            // return h.next;
-            if (head == null)
-            {
-                return null;
-            }
-            if (head.val == val)
-            {
-                return RemoveElements(head.next, val);
-            }
-            else
-            {
-                head.next = RemoveElements(head.next, val);
-            }
-            return head;
-        }
+        
 
         //205.同构字符串
         //https://leetcode-cn.com/problems/isomorphic-strings/
